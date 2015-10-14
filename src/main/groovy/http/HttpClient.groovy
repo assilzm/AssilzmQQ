a579319c24e1945a1845eef377f824c66f85afdd
assilzm <assilzm@gmail.com>
2015-10-14 18:22:59 +0800

    update

--- src/main/groovy/http/HttpClient.groovy
+++ src/main/groovy/http/HttpClient.groovy
@@ -3,9 +3,11 @@ package http
 import com.ning.http.client.AsyncHttpClient
 import com.ning.http.client.Param
 import com.ning.http.client.cookie.Cookie
+import com.ning.http.client.cookie.CookieDecoder
 import com.ning.http.client.resumable.ResumableAsyncHandler
 import com.ning.http.client.resumable.ResumableListener
 
+import javax.xml.ws.AsyncHandler
 import java.nio.ByteBuffer
 
 /**
@@ -18,7 +20,11 @@ class HttpClient {
 
     final static String USER_AGENET = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
 
-    static List<Cookie> cookies = new ArrayList<>()
+    List<Cookie> cookies = new ArrayList<>()
+
+    String referrer = null
+
+    Map<String,String> headerToAdd = [:]
 
 
     AsyncHttpClient client
@@ -29,7 +35,9 @@ class HttpClient {
     }
 
     String get(String url) {
+        println url
         getBody(client.prepareGet(url))
+
     }
 
     String post(String url, Map<String, String> params) {
@@ -40,20 +48,36 @@ class HttpClient {
         getBody(client.preparePost(url), params)
     }
 
-    String getBody(AsyncHttpClient.BoundRequestBuilder request, List<Param> params = null) {
-        def response = request.addHeader("USER_AGENET", USER_AGENET).addQueryParams(params).setCookies(cookies).execute().get()
-        println "request:" + this.cookies
-        println "response:" + response.cookies
-        syncCookies(response.cookies)
+    String getBody(AsyncHttpClient.BoundRequestBuilder request, List<Param> params = null, AsyncHandler asyncHandler = null) {
+        println this.cookies
+        def response = addHeader(request.setFollowRedirects(true).addHeader("USER-AGENET", USER_AGENET).addHeader("Referrer", referrer).addHeader("Accept", "*/*").addQueryParams(params).setCookies(cookies)).execute().get()
+        println response.headers
+        println response.cookies
+        syncCookies(response.headers.get("Set-Cookie"))
         response.responseBody
     }
 
-    static void syncCookies(List<Cookie> cookies) {
-        this.cookies.clear()
-        cookies.each {
-            this.cookies.add(it)
+    AsyncHttpClient.BoundRequestBuilder addHeader(AsyncHttpClient.BoundRequestBuilder requestBuilder) {
+        headerToAdd.each { key, value ->
+            requestBuilder.addHeader(key, value)
+        }
+
+        headerToAdd.clear()
+        return requestBuilder
+    }
+
+    void syncCookies(List<String> setCookie) {
+        setCookie.each { cookieString ->
+            if (cookieString) {
+                cookieString.split(";,").each {
+                    Cookie cookie = CookieDecoder.decode(it)
+                    if (!(cookie in cookies))
+                        cookies.add(cookie)
+                }
+            }
         }
     }
+
 
     void getFile(String url, File fileToStore) {
         if (fileToStore.exists())
@@ -75,7 +99,18 @@ class HttpClient {
                 return file.length()
             }
         })
-        client.prepareGet(url).execute(a).get()
+        def response = client.prepareGet(url).addHeader("USER-AGENET", USER_AGENET).addHeader("Referrer", referrer).addHeader("Accept", "*/*").setCookies(cookies).execute(a).get()
+        syncCookies(response.headers.get("Set-Cookie"))
+    }
+
+
+    String getCookie(String name) {
+        for (cookie in cookies) {
+            if (cookie.name == name) {
+                return cookie.value
+            }
+        }
+        return null
     }
 
     void getFile(String url, String fileToPath) {
--- src/main/groovy/qq/Login.groovy
+++ src/main/groovy/qq/Login.groovy
@@ -22,22 +22,65 @@ class Login {
     //String qqNum,String password
     void login() {
         HttpClient httpClient = new HttpClient()
-        String initPage = httpClient.get("http://w.qq.com/login.html")
-        String initUrl = RegexUtils.getRegexValue(initPage, /\.getElementById\("fr"\)\.src\s*=\s*"([^"]+)/)
+        String html = httpClient.get("http://w.qq.com/login.html")
+        String initUrl = RegexUtils.getRegexValue(html, /\.getElementById\("fr"\)\.src\s*=\s*"([^"]+)/)
         String initPageContent = httpClient.get(initUrl + "0")
         appid = RegexUtils.getRegexValue(initPageContent, /var g_appid =encodeURIComponent\("(\d+)"\);/)
         String sig = RegexUtils.getRegexValue(initPageContent, /var g_login_sig=encodeURIComponent\("(.*?)"\);/)
         String js_ver = RegexUtils.getRegexValue(initPageContent, /var g_pt_version=encodeURIComponent\("(\d+)"\);/)
         String mibao_css = RegexUtils.getRegexValue(initPageContent, /var g_mibao_css=encodeURIComponent\("(.+?)"\);/)
         File file = new File(qrcode_path)
-        httpClient.getFile("https://ssl.ptlogin2.qq.com/ptqrshow?appid=${appid}&e=0&l=L&s=8&d=72&v=4", file)
         Long startTime = System.currentTimeMillis()
-        sleep(2000)
-        println "https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=${appid}&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-${System.currentTimeMillis() - startTime}&mibao_css=${mibao_css}&t=undefined&g=1&js_type=0&js_ver=${js_ver}&login_sig=${sig}"
+        httpClient.referrer = "https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
+        httpClient.getFile("https://ssl.ptlogin2.qq.com/ptqrshow?appid=${appid}&e=0&l=M&s=8&d=72&v=4&t=0.1196356974542141", file)
+        List<String> retList=new ArrayList<>()
+        while (1) {
+            html = httpClient.get("https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=${appid}&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-${System.currentTimeMillis() - startTime}&mibao_css=${mibao_css}&t=undefined&g=1&js_type=0&js_ver=${js_ver}&login_sig=${sig}&pt_randsalt=0")
+            println html
+            retList = decodeReturnValue(html)
+            if (retList.size() > 1 && retList.get(0) == "0")
+                break
+            sleep(1000)
+        }
+        assert retList.size()==6
+        username=retList.get(5)
+        def mibaoUrl=retList.get(2)
+        sleep(1000)
+        html=httpClient.get(mibaoUrl)
+        println html
+//        def url=RegexUtils.getRegexValue(html,/proxy_iframe.src = "(.+?)"/)
+//        println url
+//        html=httpClient.get(url)
+//        println html
+//        url=RegexUtils.getRegexValue(html,/location\.href="(.+?)"/)
+//        println url
+//        html=httpClient.get(url)
+//        println html
+        ptwebqq=httpClient.getCookie("ptwebqq")
+        println ptwebqq
 
-        String loginPage = httpClient.get("https://ssl.ptlogin2.qq.com/ptqrlogin?webqq_type=10&remember_uin=1&login2qq=1&aid=${appid}&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-${System.currentTimeMillis() - startTime}&mibao_css=${mibao_css}&t=undefined&g=1&js_type=0&js_ver=${js_ver}&login_sig=${sig}")
-        println loginPage
+        httpClient.referrer="http://d.web2.qq.com/proxy.html?v=20130916001&callback=1&id=2"
+        httpClient.headerToAdd=["json":"1"]
+        while(1){
+            html=httpClient.post("http://d.web2.qq.com/channel/login2",["r":"{\"ptwebqq\":\"${ptwebqq}\",\"clientid\":${client_id},\"psessionid\":\"${psessionid}\",\"status\":\"online\"}"])
+            println html
+            sleep(1000)
+        }
         httpClient.close()
+    }
 
+   static List<String> decodeReturnValue(String loginReturnValue) {
+        def ptuiCB = ""
+        List<String> returnList = new ArrayList()
+        def m = loginReturnValue =~ /ptuiCB\(([^)]+)\)/
+        if (m.find()) {
+            ptuiCB = m.group(1)
+            println ptuiCB
+            def ptuiCBList = ptuiCB.split(",")
+            ptuiCBList.each {
+                returnList.add(it.trim().replaceAll(/^'|'$/,""))
+            }
+        }
+        return returnList
     }
 }
